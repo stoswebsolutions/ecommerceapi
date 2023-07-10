3edb1fa8ba0315273a9cb62c3c29322c620c5245
stoswebsolutions <stoswebsolutions@gmail.com>
2023-07-10 15:02:49 +0530

    Users get update delete

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
     const newUser = new User({
@@ -24,10 +25,19 @@ router.post("/login", async (req, res) => {
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
 
-        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-        password !== req.body.password && res.status(401).json("Wrong Credentials Password");
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        originalPassword !== req.body.password && res.status(401).json("Wrong Credentials Password");
 
-        res.status(200).json(user);
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        },
+            process.env.JWT_SEC,
+            { expiresIn: "3d" }
+        );
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         Error.captureStackTrace(this);
     }

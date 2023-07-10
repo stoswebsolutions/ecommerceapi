1d6531664177af0127392ff0c629c5cf00035e8d
stoswebsolutions <stoswebsolutions@gmail.com>
2023-07-10 15:22:59 +0530

    new order,get update delete and monthly income

--- ecommerce.js
+++ ecommerce.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const productRoute = require("./routes/product")
 const cartRoute = require("./routes/cart")
+const orderRoute = require("./routes/order")
 dotenv.config();
 
 
@@ -18,6 +19,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(4000, err => {
     if (!err) {

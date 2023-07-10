4ca7cdc7255202c52d5e08221afdbd4609798c7c
stoswebsolutions <stoswebsolutions@gmail.com>
2023-07-10 15:20:59 +0530

    cart add update get delete

--- ecommerce.js
+++ ecommerce.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/user")
 const productRoute = require("./routes/product")
+const cartRoute = require("./routes/cart")
 dotenv.config();
 
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(4000, err => {
     if (!err) {
